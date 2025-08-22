6343eca094e1f071460d698c301dd60e365db299
Shinjon Chakraborty <toshinjonc2006@gmail.com>
2025-08-22 19:06:12 +0530

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import http from "http";
+const app = express();
+
+const server = http.createServer(app);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+server.listen(3000, () => {
+  console.log("server running on port http://localhost:3000/");
+});
