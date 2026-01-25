f18168778b880513c9f26b669b66988b627867dc
Shinjon Chakraborty <toshinjonc2006@gmail.com>
2026-01-25 23:27:33 +0530

    refactor: extreact express app

--- src/server.ts
+++ src/server.ts
@@ -1,46 +1,6 @@
-import express, { urlencoded } from "express";
+import app from "./app";
 import { PORT } from "./config/env";
-import helloRoutes from "./routes/helloRoutes";
-import moviesRoutes from "./routes/moviesRoutes";
-import genreRoutes from "./routes/genreRoutes";
-import healthRoutes from "./routes/healthRoutes";
-import { validateGetMovies } from "./middlewares/validateMovieQueries";
-import { logger } from "./middlewares/logEvents";
 import { connectDB } from "./config/dbConn";
-import cors from "cors";
-import errorHandler from "./middlewares/errorHandler";
-import corsOptions from "./config/corsConfig";
-import { apiKeyAuth } from "./middlewares/apiKeyAuth";
-import { limiter } from "./middlewares/rateLimiter";
-import helmet from "helmet";
-
-const app = express();
-
-// Middleware
-app.use(cors(corsOptions));
-app.use(helmet());
-
-app.use(limiter);
-app.use(logger);
-
-app.use(express.json());
-app.use(urlencoded({ extended: false }));
-
-// Routes
-app.use("/", helloRoutes);
-app.use("/", healthRoutes);
-
-app.use(apiKeyAuth);
-app.use("/api/movies", validateGetMovies, moviesRoutes);
-app.use("/api/genres", genreRoutes);
-
-app.all(/.*/, (req, res, next) => {
-  const error = new Error(`Not Found - ${req.originalUrl}`);
-  (error as any).statusCode = 404;
-  next(error);
-});
-
-app.use(errorHandler);
 
 connectDB()
   .then(() => {
@@ -53,5 +13,5 @@ connectDB()
   .catch((err) => {
     console.error("❌ Failed to connect to MongoDB");
     console.error(err);
-    process.exit(1); // required for Render to show error logs
+    process.exit(1);
   });
